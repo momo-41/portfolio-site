3b4b718a13963c6309ccf34fb10cc96154231da6
momo-41 <minimizu41@gmail.com>
2024-07-12 13:36:56 +0900

    headerとの相性を見るためにfirst-viewを背景だけ作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <Box
+      pt={8}
+      height={"92vh"}
+      sx={{
+        backgroundImage: `url("white-background-image-large.jpg")`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        // backgroundAttachment: "fixed", //二次リリース
+      }}
+    >
+      aaa
+    </Box>
+  );
 };
 
 export default page;
