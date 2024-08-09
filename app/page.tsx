793502233898be12193e2fc27af107949221dcf6
momo-41 <minimizu41@gmail.com>
2024-08-09 13:27:39 +0900

    全てのページに背景の画像を固定するように修正

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,26 @@ import SkillsView from "./components/skills-view";
 import WorksView from "./components/works-view";
 import ProfileView from "./components/profile-view";
 import MessageView from "./components/message-view";
+import { Box } from "@mui/material";
 
 const page = () => {
   return (
     <>
-      <FirstView />
-      <AboutMeView />
-      <SkillsView />
-      <WorksView />
-      <ProfileView />
-      <MessageView />
+      <Box
+        sx={{
+          backgroundImage: `url("white-background-image-large.jpg")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <FirstView />
+        <AboutMeView />
+        <SkillsView />
+        <WorksView />
+        <ProfileView />
+        <MessageView />
+      </Box>
     </>
   );
 };
