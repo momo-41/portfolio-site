a2fef4039c185aea811506a6eaf18b36eb297298
momo-41 <minimizu41@gmail.com>
2024-08-09 12:39:12 +0900

    profile-dataの文章の修正

--- app/const/profile-data.ts
+++ app/const/profile-data.ts
@@ -11,31 +11,31 @@ export const ProfileData: ProfileDataType[] = [
     profileDate: "2019-2022",
     profileTitle: "私立日本大学藤沢高等学校 入学",
     profileDescription:
-      "部活動は水泳部のマネージャー\n自分の行動が誰かの助けになることに喜びを感じていた\n勉強を教えるのが好き\n数学が好きで理系を選択",
+      "部活動は強豪である水泳部のマネージャー\n自分の行動が誰かの助けになることに喜びを感じていた\n勉強を教えるのが好き\n数学が好きで理系を選択",
   },
   {
     profileDate: "2022-現在",
     profileTitle: "成蹊大学 理工学部 応用化学専攻 入学",
     profileDescription:
-      "化学の視点での「色」に興味を持ったため化学を専攻\n現在は化学分野を幅広く学んでいる\n文系の講義も学ぶことができ楽しい",
+      "化学の視点での「色」に興味を持ったため化学を専攻\n現在は化学分野を幅広く学んでいる\n文系の講義も学ぶことができて楽しいと感じている",
   },
   {
     profileDate: "2022-現在",
     profileTitle: "アルバイトを始める",
     profileDescription:
-      "勉強を教えるのが好きなことを活かして塾講師になる\n完璧主義を活かして結婚式場のスタッフになる\n人のために行動していることにやりがいを感じている",
+      "勉強を教えるのが好きなことを活かして塾講師になる\n完璧主義な気質を活かして結婚式場のスタッフになる\n人のために行動することにやりがいを感じている",
   },
   {
     profileDate: "2023/10-現在",
     profileTitle: "成蹊大学エンジニア団体 PeachTechに加入",
     profileDescription:
-      "友人の紹介をきっかけにして\n大学生のためのアプリが欲しいと思い立ち加入\nプログラミングに苦手意識があったため\n初めはwebデザインを担当",
+      "大学生のためのアプリが欲しいと思い加入\nただプログラミングに苦手意識があったため\n初めはwebデザインを担当",
   },
   {
     profileDate: "2024/2-現在",
     profileTitle: "Webのプログラミングを始める",
     profileDescription:
-      "Youtubeを参考にHTML/CSSでポートフォリオサイト\nそしてJavaScriptとReactでtodoリストを1つずつ開発\nその後はWorksに記載したようにプロダクトを開発",
+      "Youtubeを参考にHTML/CSSでポートフォリオサイト\nJavaScriptとReactでtodoリストを1つずつ開発\nその後はWorksに記載したようなプロダクトを開発",
   },
   {
     profileDate: "2024/3",
